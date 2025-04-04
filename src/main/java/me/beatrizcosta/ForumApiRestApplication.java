46d3313b77e5cb0971d04a67460296a8b51043cd
biacosta7 <contact.beatrizcosta@gmail.com>
2025-04-04 06:39:25 -0300

    Initial commit

---
+++ src/main/java/me/beatrizcosta/ForumApiRestApplication.java
@@ -0,0 +1,13 @@
+package me.beatrizcosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/beatrizcosta/ForumApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.beatrizcosta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
